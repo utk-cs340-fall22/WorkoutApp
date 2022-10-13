b56c9d4a22ef396da5be7badb24f8c3ebdb4aad4
Jacob Howard <jhowar63@vols.utk.edu>
2022-10-12 20:52:29 -0400

    Creating WorkoutHistory, Workout, Exercise models and migrating them to db

--- WorkoutApp/Main/apps.py
+++ WorkoutApp/Main/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Main'
+
+# jhowar63
+# A tutorial I watched did this when they added models so I am.
+# Not sure if it is necessary or correct.
+# class WorkoutAppConfig(AppConfig):
+   # name = 'WorkoutApp'
--- WorkoutApp/Main/models.py
+++ WorkoutApp/Main/models.py
@@ -1,3 +1,22 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+# jhowar63
+# Working on adding Library for exercises.
+# The general idea is: 
+# Every user will have a WorkoutHistory, that is a collection of Workouts.
+# Every workout will be a collection of Exercises.
+
+class WorkoutHistory(models.Model):
+    pass
+
+class Workout(models.Model):
+    date = models.DateField() # Date of the workout, to keep track chronologically
+
+class Exercise(models.Model):
+    # Should link exercises to larger workout.
+    # on_delete, should delete all exercises associated with workout if workout is deleted.
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
