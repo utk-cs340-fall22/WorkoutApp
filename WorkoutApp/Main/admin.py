774e477886bb3154d8bd7854da51536372bf1f88
NutsRobin <bowersjm116@gmail.com>
2022-11-24 11:25:05 -0600

    added CalorieHistory to admin

--- WorkoutApp/Main/admin.py
+++ WorkoutApp/Main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Workout, Exercise, WorkoutHistory
+from .models import CalorieHistory, Workout, Exercise, WorkoutHistory
 
 # Register your models here.
 admin.site.register(WorkoutHistory)
 admin.site.register(Workout)
 admin.site.register(Exercise)
+admin.site.register(CalorieHistory)
